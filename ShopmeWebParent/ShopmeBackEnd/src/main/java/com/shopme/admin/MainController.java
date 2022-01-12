cf449fa461553abaab3a52a3316ba5731101dc71
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2022-01-12 13:21:28 +0100

    Task 5: create ShopmeBackEnd module (backend admin control)

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/MainController.java
@@ -0,0 +1,20 @@
+package com.shopme.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @Created 12/01/2022 - 13:07
+ * @Package com.shopme.admin
+ * @Project ShopmeProject
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String viewHomePage() {
+        return "index";
+    }
+}
