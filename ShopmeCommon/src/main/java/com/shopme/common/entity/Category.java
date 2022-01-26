477f2a7e646a93cc9ef3a3d5aae89ecf500fdec5
ouakala <mr.ouakala.abdelaaziz@gmail.com>
2022-01-26 09:50:38 +0100

    Task 41: create category entity & repository

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -0,0 +1,84 @@
+package com.shopme.common.entity;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name",nullable = false,length =128 )
+    public String name;
+    @Column(name = "alias",nullable = false,length = 64)
+    public String alias;
+    @Column(name = "image",nullable = false,length = 128)
+    public String image;
+    @Column(name = "enabled")
+    public boolean enabled;
+
+    @OneToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private Set<Category> chiidren = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+
+    public Set<Category> getChiidren() {
+        return chiidren;
+    }
+
+    public void setChiidren(Set<Category> chiidren) {
+        this.chiidren = chiidren;
+    }
+}
